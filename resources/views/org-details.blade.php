dd08383bf7ca3ed12007f80491bf6bf3d2d791a8
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2017-08-14 20:42:59 +0530

    trivial

--- resources/views/org-details.blade.php
+++ resources/views/org-details.blade.php
@@ -51,11 +51,12 @@ IFSC Code: IBKL0000457</td>
           </tbody>
         </table>
 
-      <p class="is-size-7">Logo designed by <strong>Kanupriya Sharma</strong>.</p>
+
         <figure>
           <img src="logo.png">
 	</figure>
 
+      <p class="is-size-7">Logo designed by <strong>Kanupriya Sharma</strong>.</p>
 </div>
 </div>
 @endsection
