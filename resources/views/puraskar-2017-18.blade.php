182c21e437a3b1e645a49292f81b3d5b0947a88b
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2018-12-13 21:02:58 +0530

    incorrect year

--- resources/views/puraskar-2017-18.blade.php
+++ resources/views/puraskar-2017-18.blade.php
@@ -13,7 +13,7 @@
 	<div class="content has-text-left">
 		<p>विश्वस्त पुरस्कार is an award based on trusteeship concept of Mahatma Gandhi. Pesron who works for society while considering himself as a trustee of his own wealth, intelligence, skills is a matter of respect for SPARK. SPARK facilitates such person with  विश्वस्त पुरस्कार which consist of Trophy, Certificate of honor and cash as a token of gratitude and respect.</p>
 	<h3>2017-18</h3>
-	<p>Award for the year 2016-17 was given to Mr. Dilip Kulkarni and Mrs. Pournima Kulkarni. The Kulkarni couple has been living in Kudawale, a quiet village in the Konkan region of Maharashtra for the last 25 years championing an environment friendly lifestyle and sustainable development by being a living example for the same.</p>
+	<p>Award for the year 2017-18 was given to Mr. Dilip Kulkarni and Mrs. Pournima Kulkarni. The Kulkarni couple has been living in Kudawale, a quiet village in the Konkan region of Maharashtra for the last 25 years championing an environment friendly lifestyle and sustainable development by being a living example for the same.</p>
 	</div>
 
 	<figure class="column has-text-centered">
