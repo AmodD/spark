29fe00de7736e003abf4dcce027f4dbf2a45c0e4
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2020-02-12 05:31:55 +0530

    latest images

--- resources/views/projects-2019.blade.php
+++ resources/views/projects-2019.blade.php
@@ -14,6 +14,9 @@
 	<div class="content has-text-left">
 		<h3>Treadle Pumps</h3>
 		<p>Treadle pump is an innovation of CTARA-IIT(B) which is helpful for small scale irrigation.Treadle pump is operated manually.It can fetch water from 23ft depth and can deliver it up to 35ft height. SPARK donated 2 such pumps to farmers in village Aamlon-Peth.Nashik</p>
+	<figure class="column has-text-centered">
+		<img src="Treadle_pump_1.jpg">
+	</figure>
 		<ul><strong>Seeds</strong>
 			<li>Villagers at Aamlon are very enthusiastic and ready for experiments.</li>
 			<li>SPARK handed over 2 different types of seeds of paddy ( जावयाची गुंडी,झिनी – 50Kg each).</li>
@@ -21,6 +24,12 @@
 			<li>Few farmers got yield of 1 Quintal per 1 Kg which the highest yield in that area.</li>
 			<li>SPARK also handed over kitchen-garden kits (seeds of vegetables having high nutrition value) to farmers in Aamlon</li>
 		</ul>
+	<figure class="column has-text-centered">
+		<img src="Seeds_Outcome_1.jpg">
+		<img src="Seeds_Outcome_2.jpg">
+    		<img src="Seeds_delivery__1.jpg">
+		<figcaption>Seeds Delivery</figcaption>
+	</figure>
 	</div>
 
 <article class="message column">
@@ -29,17 +38,17 @@
   </div>
   <div class="message-body">
 	<figure class="has-text-centered">
-		<p><img src="Pimpalvati-10x10.jpg"></p>
-		<p>Farm Pond Size - 10x10x3 , Capacity 300000 Liters, Use – Percolation,Harwesting,Drinking water for cattle</p>
+		<p><img src="Farm_pond_1-10x10x3.jpg"></p>
+		<p>Farm Pond Size - 10x10x3 , Capacity 300000 Liters, Use – Percolation,Harvesting,Drinking water for cattle</p>
 		<br>
-    		<img src="Aamlon_10x10x3_Farmpond.jpg">
-		<p>Farm Pond Size - 10x10x3 , Capacity 300000 Liters, Use – Percolation,Harwesting,Drinking water for cattle</p>
+    		<img src="Farm_pond_2-10x10x3.jpg">
+		<p>Farm Pond Size - 10x10x3 , Capacity 300000 Liters, Use – Percolation,Harvesting,Drinking water for cattle</p>
 		<br>
-		<img src="Salwan_Buldhana_10x10x3.jpg">
-		<p>Farm Pond Size - 10x10x3 , Capacity 300000 Liters, Use – Harwesting,Drinking water for cattle</p>
+		<img src="Farm_pond_3-10x10x3.jpg">
+		<p>Farm Pond Size - 10x10x3 , Capacity 300000 Liters, Use – Harvesting,Drinking water for cattle</p>
 		<br>
-    		<img src="Chichwadi_cement_damCompletion_day_with_banner.jpg">
-		<p>Farm Pond Size - 20x20x2 , Capacity 400000 Liters, Use – Harwesting,Drinking water for cattle</p>
+    		<img src="Village_pond_20x20x3.jpg">
+		<p>Farm Pond Size - 20x20x2 , Capacity 400000 Liters, Use – Harvesting,Drinking water for cattle</p>
 		<br>
 	</figure>
   </div>
@@ -52,7 +61,9 @@
   <div class="message-body">
 	<h6 class="title is-6 has-text-centered">20 Hectares Continuous Contour Trenches (CCT), 3 Hectares of Deep Continuous Contour Trenches (DCCT)<h6>
 	<figure class="column has-text-centered">
-    		<img src="Zatamziri_graded_contour_bunding.jpg">
+    		<img src="Farm_bund_CCT.jpg">
+    		<img src="Farm_bund_CCT_5.jpg">
+    		<img src="Farm_bund_CCT_6.jpg">
 		<p>Use – Water percolation by reducing velocity of water running from ridge area and reducing soil erosion.</p>
 	</figure>
   </div>
@@ -65,7 +76,8 @@
   <div class="message-body">
 	<h6 class="title is-6 has-text-centered">Loose Boulder Checks(LBC)<h6>
 	<figure class="column has-text-centered">
-    		<img src="Aamlon_Farmbunding.jpg">
+    		<img src="Loose_Boulder_Check_1.jpg">
+    		<img src="Loose_Boulder_Check_2.jpg">
 		<p>Use – Water percolation by reducing velocity of water running from ridge area and reducing soil erosion.</p>
 	</figure>
   </div>
